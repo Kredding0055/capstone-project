6a8b2975bb70663db6d8cfb86dd0ab161c48114d
Kredding0055 <redkr3815@gmail.com>
2025-01-07 19:10:03 -0800

    adjusted backend login and signup forms

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,7 @@
 from .db import db
 from .user import User
+from .motorcycle import Motorcycle, MotorcycleImages
+from .favorite import Favorite
+from .review import Review
+from .shopping_cart import ShoppingCart
 from .db import environment, SCHEMA
