5b6030e15b22b6911e787ec740b7a067d2318316
Kredding0055 <redkr3815@gmail.com>
2025-01-09 10:20:56 -0800

    updated the favorite model

--- app/models/favorite.py
+++ app/models/favorite.py
@@ -5,15 +5,20 @@ from sqlalchemy import func
 class Favorite(db.Model):
   __tablename__ = 'favorites'
 
+  # if environment == "production":
+  #   __table_args__ = {'schema': SCHEMA}
+
   if environment == "production":
-    __table_args__ = {'schema': SCHEMA}
+    __table_args__ = {'schema': SCHEMA, 'table_args': (db.UniqueConstraint('user_id', 'motorcycle_id'),)}
+  else:
+    __table_args__ = (db.UniqueConstraint('user_id', 'motorcycle_id'),)
 
   id = db.Column(db.Integer, primary_key=True)
   user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
   motorcycle_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('motorcycles.id')), nullable=False)
   created_at = db.Column(db.DateTime, server_default=func.now())
 
-  __table_args__ = (db.UniqueConstraint('user_id', 'motorcycle_id'),)
+  # __table_args__ = (db.UniqueConstraint('user_id', 'motorcycle_id'),)
 
 
   def to_dict(self):
